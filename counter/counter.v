2ac67a5727d514a2c3835436a93eb105f5348ff6
kkozoriz <kkozoriz7@yandex.ru>
2024-05-12 13:35:41 +0300

    counter.v added

---
+++ counter/counter.v
@@ -0,0 +1,15 @@
+module counter(
+    input clk,
+    output clk2
+);
+    assign clk2 = (cnt == 9);
+    reg [3:0] cnt = 0;
+
+    always @(posedge clk) begin
+        if (clk2)
+            cnt <= 0;
+        else
+            cnt <= cnt+1;
+    end
+
+endmodule
